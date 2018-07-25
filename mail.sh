cd93ffae425f6db13bfd7cb0f9b966500a957367
Nurlan199206 <user_1771@mail.ru>
2018-07-25 10:36:46 +0600

    Update mail.sh

--- mail.sh
+++ mail.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
-let kol=`cat /home/inspektor/chestnoe/list | wc -l`
+let kol=`cat /home/user/test/list | wc -l`
 let i=0
-array=(`cat /home/inspektor/chestnoe/list`)
+array=(`cat /home/user/test/list`)
 while [ "$i" -lt "$kol" ]
 do
   f=${array[$i]}
      echo "Отправлено письмо на $f"
-     mutt -e "set content_type=text/html" $f -s "Акция" < chestnoe.html
+     mutt -e "set content_type=text/html" $f -s "Акция" < test.html
   i=$(($i+1))
 done
 exit 0
